974bb40716e310f6dfaa5c9647dabdead4c9d88b
CoderNandan <nandanindia1947@gmail.com>
2026-03-07 22:15:29 +0530

    add main.go with variable declaration examples

---
+++ 03_variables/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+
+	// infer
+	// var name = "golang"
+	// var isAdult bool = true
+
+	// var age int = 30
+
+	// shorthand syntax
+	// name := "golang"
+	name := "golang"
+
+	fmt.Println(name)
+
+	// var name string
+	// name = "golang"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	// price := 50.5
+
+	// fmt.Println(price)
+}
